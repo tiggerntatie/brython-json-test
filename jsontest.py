f38aaf3180125744e446c26a2eeb46d90aaf4e17
Eric Dennison <ericd@netdenizen.com>
2016-12-21 13:42:17 -0500

    Updated from Brython Server: 12/21/2016 1:42:17 PM

--- jsontest.py
+++ jsontest.py
@@ -2,5 +2,13 @@ import json
 
 with open("rates.json") as f:
     j = json.load(f)
-    print(j)
     ## j is a dictionary type with all of the json data in it
+    for x in j:
+        print(x)
+    for x in j['list']:
+        print(x)
+    #for x in j['list']['resources']:
+
+    #print(j['list']['meta'])
+    #print(j['list']['resources'])
+    print(type(j['list']['resources'])

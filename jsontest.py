1f00af8563c3fa1827568c34e7360cee304cd345
Eric Dennison <ericd@netdenizen.com>
2016-12-21 14:01:06 -0500

    Updated from Brython Server: 12/21/2016 2:01:07 PM

--- jsontest.py
+++ jsontest.py
@@ -3,4 +3,15 @@ import json
 with open("rates.json") as f:
     j = json.load(f)
     ## j is a dictionary type with all of the json data in it
-    print(j)
+    ## prints the country that rates are with respet to (base)
+    print(j['base'])
+    ## prints the dictionary of all rates
+    print(j['rates'])
+    ## prints the exchange rate for a single country, relative to USD
+    print(j['rates']['AUD'])
+    ## print all of the countries available:
+    for country in j['rates']:
+        print(country)
+    ## make a list of countries
+    countries = [country for country in j['rates']]
+    print(countries)
